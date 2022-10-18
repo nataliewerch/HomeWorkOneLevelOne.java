e4d7c5fa55efb4b7dc79adf73c55061f00460be0
Natalie Werch <natalie@Natalie-iMac.fritz.box>
2022-10-18 19:28:21 +0200

    Initial commit

---
+++ src/HomeWork/HomeWorkOneLevelOne.java
@@ -0,0 +1,27 @@
+package HomeWork;
+
+public class HomeWorkOneLevelOne {
+    public static void main(String[] args) {
+        char c = 71;
+        int i = 89;
+        byte b = 4;
+        short s = 56;
+        float f = 4.7333436f;
+        double d = 4.355453532;
+        long l = 12121l;
+        boolean bl = true;
+
+        System.out.println("char: " + c);
+        System.out.println("integer: " + i);
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("long: " + l);
+        System.out.println("boolean: " + bl);
+
+
+    }
+
+    }
+
---
+++ src/HomeWork/HomeWorkOneLevelTwo.java
@@ -0,0 +1,11 @@
+package HomeWork;
+
+public class HomeWorkOneLevelTwo {
+    public static void main(String[] args) {
+        int n = 765;
+        System.out.println("s = " + n / 100);
+        System.out.println("d = " + n / 10 % 10);
+        System.out.println("e = " + n % 10);
+
+    }
+}
